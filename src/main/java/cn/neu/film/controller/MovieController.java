d3ffbb5d7b8e6561cbb2a0485d0a4f27a1d9c7a4
Zigior <1103051218@qq.com>
2019-12-10 15:21:41 +0800

    first

--- src/main/java/cn/neu/film/controller/MovieController.java
+++ src/main/java/cn/neu/film/controller/MovieController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+//github提交成功了吗???
 
 @RestController
 public class MovieController {
